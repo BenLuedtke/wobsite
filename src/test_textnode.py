053a57703ac420da8d6904a6739594a4e4c66f78
BenLuedtke <ben@bluedtke.com>
2025-07-25 12:16:15 -0500

    Completed build

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,9 @@
 import unittest
 
-from textnode import TextNode, TextType
-
+from textnode import (TextNode, 
+TextType, 
+text_node_to_html_node,
+)
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
@@ -14,13 +16,13 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_eq_3(self):
-        node = TextNode("This is a text node", TextType.NORMAL, url="www.google.com")
-        node2 = TextNode("This is a text node", TextType.NORMAL, url="www.google.com")
+        node = TextNode("This is a text node", TextType.TEXT, url="www.google.com")
+        node2 = TextNode("This is a text node", TextType.TEXT, url="www.google.com")
         self.assertEqual(node, node2)
 
     def test_eq_4(self):
-        node = TextNode("This is a text node", TextType.NORMAL, url="www.google.com")
-        node2 = TextNode("This is a text node", TextType.NORMAL)
+        node = TextNode("This is a text node", TextType.TEXT, url="www.google.com")
+        node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertNotEqual(node, node2)
 
     def test_eq_5(self):
@@ -28,6 +30,38 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_2(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'b')
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_3(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'i')
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_4(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'code')
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_5(self):
+        node = TextNode("This is a text node", TextType.LINK, url='www.google.com')
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'a')
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props, {"href": 'www.google.com'})
+
+    
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,10 @@
 from enum import Enum
+import re
+
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "Normal text"
+    TEXT = "Normal text"
     BOLD = "**Bold text**"
     ITALIC = "_Italic text_"
     CODE = "`Code text`"
@@ -22,3 +25,25 @@ class TextNode():
 
     def __repr__(self):            
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+        
+    match TextType(text_node.text_type):
+        case TextType.TEXT:
+            return LeafNode(tag=None,value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag='b',value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag='i',value=text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag='code',value=text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+
+        case _: 
+            raise Exception('Text node does not have a valid text type.')
+
+
